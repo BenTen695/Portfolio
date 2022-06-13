d8c1680c985c0657ad362e6ef0275ed9117d5edc
BenTen695 <103340172+BenTen695@users.noreply.github.com>
2022-06-13 17:27:08 +0100

    rev2

--- app.py
+++ app.py
@@ -1,36 +1,47 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
-app = Flask(__name__)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+#create the object of Flask
+app  = Flask(__name__)
 
-# routes
-@app.route("/")
+#creating our routes
+@app.route('/')
 def Index():
     return render_template('index.html')
 
-@app.route('/home')
-def home():
-    return render_template('home.html')
-
 @app.route('/cv')
-def cv():
+def CV():
     return render_template('cv.html')
 
-@app.route('/akita about')
-def akita_about():
-    return render_template('akita_about.html')
+@app.route('/akita_aboutcopy')
+def akita_aboutcopy():
+    return render_template('akita_aboutcopy.html')    
 
-@app.route('/akita further reading')
-def akita_further_reading():
+@app.route('/test')
+def test():
+    return render_template('test.html')
+
+@app.route('/cv_copy')
+def cv_copy():
+    return render_template('cv_copy.html')
+
+@app.route('/mythology_greek')
+def mythology_greek():
+    return render_template('mythology_greek.html')
+
+
+@app.route('/akita_furtherreading')
+def Further_Reading():
     return render_template('akita_further_reading.html')
 
-@app.route('/engineering hazardous area tool')
-def engineering_hazardous_area_tool():
-    return render_template('engineering_hazardous_area_tool.html.html')
-
-@app.route('/engineering tools')
+@app.route('/engineering_tools')
 def engineering_tools():
     return render_template('engineering_tools.html')
-# /Routes
+
+@app.route('/engineering_hazardous_area_tool')
+def engineering_hazardous_area_tool():
+    return render_template('engineering_hazardous_area_tool.html')
+
+#run flask app
+if __name__ == "__main__":
+    app.run(debug=True)
